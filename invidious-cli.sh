bc1404376c6c0b6a9f922ad6089565f2fe2e4719
Andoni Perales <94477827+andoniperales@users.noreply.github.com>
2022-10-20 18:25:18 +0200

    Update invidious-cli.sh

--- invidious-cli.sh
+++ invidious-cli.sh
@@ -1,34 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Enter your search query"
-read query
-query_s=$(echo "$query" | sed 's/\s/+/g')
+IFS=$'\n'
 
-video=$(curl "https://invidious.namazso.eu/api/v1/search/?q=$query_s" -s --retry-all-errors)
-list=$(echo "$video" | grep -o -P '"title":"(.+?)","videoId"' | sed s/'"title":"'// | sed s/'","videoId"'// | sed 's:\\::g' | nl -b a)
-echo "$list"
+while :; do 
+    echo "Enter your search query"
+    read query
 
-echo -e "\nEnter the video number"
+    query=$(
+        echo $query | 
+        sed 's/\s/+/g'
+    )
+    
+    results=$(curl "https://invidious.namazso.eu/api/v1/search/?q=$query" -s --retry-all-errors)
 
-while :; do
+    titles=$(
+        echo "$results" | 
+        grep -oP '(?<="title":")(.+?)(?=","videoId")' | 
+        nl -b a
+    )
+    
+    arr_t=($titles)
+    
+    video_ids=$(
+        echo $results | 
+        grep -oP '(?<="videoId":")[^"]+?(?=")'
+    )
 
-read number
-video_id=$(echo $video | grep -o -E '"videoId":"[^"]+"' | sed -n ''''$number'p''' | sed 's/"videoId"://' | sed 's/"//')
+    arr_v=($video_ids)
 
-echo -e "\nDo you want to watch it or download it? [w(atch)/d(ownload)]"
-read opt
-if [[ $opt == "d" ]]; then
-    yt-dlp -f 22 https://yewtu.be/watch?v=$video_id
-elif [[ $opt == "w" ]]; then
-    mpv https://yewtu.be/watch?v=$video_id --fs --ytdl-format=22
-else 
-    echo -e "\nWrong input, try again"
-    sleep 1
-fi
-
-clear
-
-echo "$list"
-echo -e "\nEnter another number, or quit by pressing Ctrl+C"
-
+    for (( n=0;;))
+    do
+        clear
+        echo -e "Results:"
+        echo ${arr_t[$n]}
+        echo -e "\n n(ext)/p(revious/w(atch)/d(ownload)/s(earch)"
+        read opt 
+        case $opt in
+            n | N) clear; (( n++ )); continue;;
+            p | P) clear; (( n-- )); continue;;
+            w | W) mpv https://yewtu.be/watch?v=${arr_v[$n]} --fs; clear;;
+            d | D) yt-dlp -f 22 https://yewtu.be/watch?v=${arr_v[$n]}; clear;;
+            s | S) clear; break;;
+            *) echo "Wrong input; try again"; sleep 1; clear; continue;;
+        esac
+    done 
+    
 done
