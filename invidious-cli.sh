eae1ef6f0cab6115f8476ced5a2898b75498ef41
Andoni <andonifp@pm.me>
2024-12-18 01:06:04 +0100

    Fixed it and revamped it. Now it works (save for 500 errors from instances)

--- invidious-cli.sh
+++ invidious-cli.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
+IFS=$' '
+
+arr_inst=(https://yewtu.be https://inv.nadeko.net https://invidious.jing.rocks)
+
+for i in ${!arr_inst[@]}; do
+    instance_response=$(curl --head "${arr_inst[$i]}/api/v1/search?q=test" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" -s | head -n 1 | grep -oP '\d{3}')
+    case $instance_response in
+        200) instance=${arr_inst[$i]}; break;;
+        *) :;;
+    esac
+done
+
+INV_API="$instance/api/v1/search"
 IFS=$'\n'
-
-instances=(
-yewtu.be vid.puffyan.us yt.artemislena.eu invidious.flokinet.to invidious.projectsegfau.lt invidious.slipfox.xyz invidious.privacydev.net iv.melmac.space iv.ggtyler.dev invidious.lunar.icu inv.nadeko.net inv.tux.pizza invidious.protokolla.fi iv.nboeck.de invidious.private.coffee yt.drgnz.club iv.datura.network invidious.fdn.fr invidious.perennialte.ch youtube.owacon.moe
-)
-
-rand=$(
-        echo $((RANDOM % 19))
-)
-
-instance=$(
-        echo ${instances[$rand]}
-)
-
-INV="https://$instance" 
-INV_API="$INV/api/v1/search"
 
 while :; do
         echo "Enter your search query:"
@@ -26,7 +24,7 @@ while :; do
                 sed 's/\s/+/g'
         )
 
-        results=$(curl "$INV_API/?q=$query" -s --retry-all-errors)
+        results=$(curl "$INV_API/?q=$query" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" -s --retry-all-errors)
 
         titles=$(
                 echo $results | 
@@ -45,24 +43,26 @@ while :; do
         while :; do
                 echo "$titles"
                 
-                echo -e "\nEnter the number of the video you want to watch ('s' to do a new search):"
+                echo -e "\nEnter the number of the video you want to watch ('s' to do a new search, 'q' to quit):"
                 
                 read n
                 
-                VID="$INV/watch?v=${arr_v[$n-1]}"       
+                VID="$instance/watch?v=${arr_v[$n-1]}"       
                 
                 case $n in
                 s | S) break;;
+                q | Q) exit;;
                 esac
 
-                echo "Now enter 'w' to watch, 'd' to download, or 's' to do a new search:"
+                echo "Now enter 'w' to watch, 'd' to download, 's' to do a new search, 'q' to quit:"
 
-                read opt
+                read option
 
-                case $opt in
+                case $option in
                 w | W) mpv $VID --fs; clear;;
                 d | D) yt-dlp $VID; clear;;
                 s | S) break;;
+                q | Q) exit;;
                 *) echo "Wrong input; try again"; sleep 1; clear; continue;;
                 esac
         done
